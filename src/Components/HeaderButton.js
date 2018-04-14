3a160db3e94135c585ef78af6389c7dcb1ac3d41
Zach Osman <zosman@gmu.edu>
2018-04-14 16:01:21 -0400

    adding images + other stuff?

--- src/Components/HeaderButton.js
+++ src/Components/HeaderButton.js
@@ -2,19 +2,22 @@ import React, { Component } from 'react';
 import { Text, View, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import { detailsData } from '../../data/details';
 
+let images = [
+    require('../../resources/learn.png'),
+    require('../../resources/prevent.png'),
+    require('../../resources/act.png'),
+    require('../../resources/donate.png'),
+];
+
 export class HeaderButton extends Component {
     state = {};
+
     render() {
         return (
             <TouchableOpacity
                 style={styles.headerButton}
                 onPress={() => this.props.navigate('Details', { data: detailsData[this.props.data.name] })}>
-                <Image
-                    style={{ width: 150, height: 150 }}
-                    source={{
-                        uri: 'https://picsum.photos/200/300/?gravity=east',
-                    }}
-                />
+                <Image style={{ width: 170, height: 170 }} source={images[this.props.data.key]} />
             </TouchableOpacity>
         );
     }
--- src/Details.js
+++ src/Details.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, StyleSheet, View, ScrollView } from 'react-native';
+import { H1, H2, H3, H4 } from './Components/Headers';
 
 export class Details extends Component {
-    static navigationOptions = {
-        header: null,
-    };
     constructor(props) {
         super(props);
         this.state = {};
     }
+    static navigationOptions = ({ navigation }) => ({
+        // header: null,
+        title: `${navigation.state.params.data.name}`,
+    });
     render() {
+        const { params } = this.props.navigation.state;
         const { navigate } = this.props.navigation;
+        // console.warn(params.data.paragraph);
+        // console.warn(params.data.h1);
         return (
             <SafeAreaView>
-                <Text> Hi! </Text>
+                <ScrollView>
+                    <H1 h1={params.data.h1} />
+                    <H2 h2={params.data.h2} />
+                    <H2 h3={params.data.h3} />
+
+                    <View style={styles.paragraph}>
+                        <Text style={styles.paragraphText}>{params.data.paragraph}</Text>
+                    </View>
+                </ScrollView>
             </SafeAreaView>
         );
     }
 }
+const styles = StyleSheet.create({
+    paragraph: { paddingLeft: 20, paddingRight: 20 },
+    h1: {
+        fontSize: 50,
+    },
+    h2: {
+        fontSize: 25,
+    },
+    paragraphText: {
+        fontSize: 18,
+    },
+});
