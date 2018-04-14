cb0fb18bac10bedd67ae9ec9f535b3a6b630c3fc
Rae Dambach <rae.dambach@gmail.com>
2018-04-14 15:47:37 -0400

    Text for Donate, Learn, Prevent, and Act

--- data/details.js
+++ data/details.js
@@ -2,29 +2,57 @@ export let detailsData = {
     learn: {
         name: "Learn",
         isAct: false,
-        paragraph:
-            'What is human trafficking? Human trafficking is defined as the act of compelling a person to engage in sexual acts or forced labor. This allows the trafficker to profit from the exploitation while continuing to manipulate the victims. This is modern day slavery and has reached near epidemic levels in the United States. Just Ask works wo counteract these growing numbers through education and awareness.',
+        paragraph: 'What is human trafficking? Human trafficking is defined as the act of compelling a person to engage in sexual acts or forced labor. This allows the trafficker to profit from the exploitation while continuing to manipulate the victims. This is modern day slavery and has reached near epidemic levels in the United States. Just Ask works wo counteract these growing numbers through education and awareness.',
+        h2: 'Where Does Human Trafficking Occur?',
+        paragraph: 'Like any crime, human trafficking can occur in any community where there are teens to manipulate and an illegal black market place to service. In 2016 there were over 7,500 cases of human trafficking reported in the United States according to the National Human Trafficking Hotline.',
+        paragraph: 'This is only a small percentage of the actual occurrences of trafficking as many cases go unreported. There are so many unreported cases because most of us wouldn’t recognize trafficking if we saw it. This map (courtesy of Polaris Project) shows where these phone calls originated from. Visit National Human Trafficking Hotline website to get statistics specific to your state:',
+        image: ''
     },
     prevent: {
         name: "Prevent",
         isAct: false,
-        h1: `You can help prevent Human Trafficking by knowing the signs to stop the abuse.
-        
-        Members of the community are our greatest assets in the fight to end human trafficking. The best way to be effective is by being able to identify the patterns and signs of abuse. Victims can be difficult to identify, so know the signs`
+        h1: 'You can help prevent Human Trafficking by knowing the signs to stop the abuse.'
+        paragraph: 'Members of the community are our greatest assets in the fight to end human trafficking. The best way to be effective is by being able to identify the patterns and signs of abuse. Victims can be difficult to identify, so know the signs.',
         youngPeople: {
-            h1: "know the Patterns of Traffickers"
+            h1: 'Know the Patterns of Traffickers',
+            h2: 'Victims can be from any gender, ethnicity, social/economic group, or level of academic achievement.',
+            h3: 'Scouting',
+            paragraph: 'Traffickers find victims via social media, shopping malls, bus stops, school, or even through friends.',
+            h3: 'Manipulating',
+            paragraph: 'Traffickers will manipulate young people by pretending to be boyfriends or girlfriends, buying presents, treating their intended victims very well, listending and acting interested in their victims lives, and even by lieing.',
+            h3: 'Trapping',
+            paragraph: 'Traffickers trick or control young people into staying in the lifestyl by false love or lies, drugs, threats of violence, or actual physical violence.'
         },
         parentsAndAdults: {
+            h1: 'The Key to Prevent is Involvement',
+            h2: 'The best way to prevent your child or loved ones from being targeted is to pay attention them and their behavior.'
+            h3: 'Know What Your Teen is Doing',
+            h3: `Be Involved in Your Teen's Life`,
+            h3: 'Talk With Your Teen About Sensitive Topic',
+            h3: 'Help Build Their Self-Esteem & Confidence',
+            h3: 'Discuss Social Media & Monitor Their Usage',
+            h3: 'Talk With Your Teen About Frienship'
+
         }
     },
     act: {
         name: "Act",
         isAct: true,
-        paragraph: '',
+        h1: `Let's Work Together to End Trafficking`,
+        paragraph: 'We need the cooperation and coordination of all of our community members to bring an end to human trafficking. By bringing awareness of the problem to your friends and family, oorganizing an event, club, or activity with the goal of educating larger groups, or simply donating to the cause, your support is what keeps the foundation going. The only way we can beat human trafficking is with the help of people like YOU!',
+        h2: 'Report an Incident of Suspected Human Trafficking',
+        incidentReport: {
+
+        }
     },
     donate: {
         name: "Donate",
         isAct: false,
-        paragraph: '',
+        h1: 'You Can Make a Difference Today'
+        paragraph: 'Your gift today provides immediate crisis care and transformative services for a homeless child in need. Here is were you can give back:',
+        h4: 'Covenant House',
+        h4: 'Blue Campaign',
+        h4: 'Worth While Wear',
+        h4: 'Unlikely Heroes'
     },
 };
