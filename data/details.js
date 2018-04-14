479bff48ed776cd98e23881d5066d98294b6251f
Rae Dambach <rae.dambach@gmail.com>
2018-04-14 15:00:04 -0400

    Added more text to Details

--- data/details.js
+++ data/details.js
@@ -1,18 +1,29 @@
 export let detailsData = {
     learn: {
+        name: "Learn",
         isAct: false,
         paragraph:
-            'What is human trafficking? Human trafficking is defined as the act of compelling a perso to engage in sexual acts or forced labor. The trafficker profits from ',
+            'What is human trafficking? Human trafficking is defined as the act of compelling a person to engage in sexual acts or forced labor. This allows the trafficker to profit from the exploitation while continuing to manipulate the victims. This is modern day slavery and has reached near epidemic levels in the United States. Just Ask works wo counteract these growing numbers through education and awareness.',
     },
     prevent: {
+        name: "Prevent",
         isAct: false,
-        paragraph: '',
+        h1: `You can help prevent Human Trafficking by knowing the signs to stop the abuse.
+        
+        Members of the community are our greatest assets in the fight to end human trafficking. The best way to be effective is by being able to identify the patterns and signs of abuse. Victims can be difficult to identify, so know the signs`
+        youngPeople: {
+            h1: "know the Patterns of Traffickers"
+        },
+        parentsAndAdults: {
+        }
     },
     act: {
+        name: "Act",
         isAct: true,
         paragraph: '',
     },
     donate: {
+        name: "Donate",
         isAct: false,
         paragraph: '',
     },
